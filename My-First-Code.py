c4a56d532f2627917fb27ca431554db1139535bc
Leonardo Neves <leoagdn@gmail.com>
2026-03-09 02:12:54 -0300

    Add welcome_message function to greet user

---
+++ My-First-Code.py
@@ -0,0 +1,7 @@
+# My first professional Hello World
+def welcome_message(name, field):
+    print(f"Hello, World! I am {name}.")
+    print(f"Currently deep-diving into {field} @ Cesupa.")
+    print(" Fluent English |  Cybersecurity | IA")
+
+welcome_message("Leonardo Neves", "Computer Science")
